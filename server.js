c469d3d6e2aafbc86116571979d40a34000c6a09
Shakil Mahammud <61838746+shakilmahammud@users.noreply.github.com>
2022-08-23 02:36:54 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
+const router = jsonServer.router('./db_todos.json');
 const PORT = process.env.PORT || 8000;
-server.use(middlewares);
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
